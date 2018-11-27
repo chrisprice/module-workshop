47fce657c21495827a5cd1235739e5198d96bbe4
Roger Peppe <rogpeppe@gmail.com>
2018-11-27 15:15:04 +0000

    v2.1.0 use v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/rogpeppe/module-workshop
+module github.com/rogpeppe/module-workshop/v2
 
 go 1.12
--- workshop.go
+++ workshop.go
@@ -1,5 +1,5 @@
 package workshop
 
-func Version() string {
-	return "v2.0.1"
+func VersionNumber() string {
+	return "v2.1.0"
 }
